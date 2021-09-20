cec45abe3489b2ad614f922cce5c53222d8dc5b5
Sumit175 <shrivastavsumit.99@gmail.com>
2021-09-20 21:56:33 +0530

    del MeetingType

--- AutomatedMeetingBookingSystem/src/main/java/com/AutomatedMeetingBookingSystem/model/MeetingType.java
+++
@@ -1,9 +0,0 @@
-package com.AutomatedMeetingBookingSystem.model;
-
-public enum MeetingType {
-	
-	CLASSROOMTRAINING("CLASSROOMTRAINING"), ONLINETRAINING("ONLINETRAINING"), CONFERENCECAL("CONFERENCECAL"), BUSINESS("BUSINESS") , CONFERENCECALL("CONFERENCECALL");
-
-	MeetingType(String string) {
-	}
-}
