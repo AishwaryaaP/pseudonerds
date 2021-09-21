42b191b3f76ed93dc6853e298211a9df36e277dc
Aishwarya Phirke <41952554+AishwaryaaP@users.noreply.github.com>
2021-09-21 20:13:34 +0530

    Update MeetingDAO.java

--- AutomatedMeetingBookingSystem/src/main/java/com/AutomatedMeetingBookingSystem/dao/MeetingDAO.java
+++ AutomatedMeetingBookingSystem/src/main/java/com/AutomatedMeetingBookingSystem/dao/MeetingDAO.java
@@ -8,14 +8,9 @@ import com.AutomatedMeetingBookingSystem.exception.ConnectionFailedException;
 import com.AutomatedMeetingBookingSystem.model.Meeting;
 
 public interface MeetingDAO {
-
-	static MeetingDAO getInstance() {
-		return new MeetingDAOImpl();
-	}
-
 	Meeting createMeeting(int organizedBy, String roomName, String title, LocalDate date, LocalTime startTime, LocalTime endTime, String type, String listOfMembers) throws ConnectionFailedException;
 
-	List<Meeting> fetchMeetingsByUserID(int userID) throws ConnectionFailedException;
+	public List<Meeting> fetchAllMeetings() throws ConnectionFailedException;
 
 	Meeting fetchMeetingByUniqueID(int uniqueID) throws ConnectionFailedException;
 }
