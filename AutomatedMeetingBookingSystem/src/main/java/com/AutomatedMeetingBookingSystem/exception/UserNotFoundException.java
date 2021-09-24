a75fa9664fb38b7f7cc9b40f68b90f0c5f5fb68c
mohitt13 <49208187+mohitt13@users.noreply.github.com>
2021-09-24 22:30:44 +0530

    Jsp Testing, Exceptions error resolved

--- AutomatedMeetingBookingSystem/src/main/java/com/AutomatedMeetingBookingSystem/exception/UserNotFoundException.java
+++ AutomatedMeetingBookingSystem/src/main/java/com/AutomatedMeetingBookingSystem/exception/UserNotFoundException.java
@@ -1,5 +1,7 @@
 package com.AutomatedMeetingBookingSystem.exception;
 
-public class UserNotFoundException {
-
+public class UserNotFoundException extends Exception {
+	public UserNotFoundException(String s) {
+		super(s);
+	}
 }
